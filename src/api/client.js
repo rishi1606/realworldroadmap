6527b70bf339ef2f118d56b84ef900251684edbe
rishi16061997 <rishi.shah@ozin.ai>
2026-05-12 19:36:06 +0530

    fix

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  envPrefix: 'PUBLIC_',
+  envPrefix: ['VITE_', 'PUBLIC_'],
 })
